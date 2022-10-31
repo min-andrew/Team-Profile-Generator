3679379efc409936f5fa85c20e64c052982830ec
Andrew Min <minandrew0711@gmail.com>
2022-10-30 19:27:40 -0700

    touched a bit of the code

--- index.js
+++ index.js
@@ -3,24 +3,6 @@ const fs = require('fs');
 const Employee = require('./lib/Employee');
 const generatehtml = require('./dist/generatehtml')
 
-function writeToFile(fileName, data) {
-    fs.appendFile(fileName, data, (err) => err ? console.error(err) : console.log('Generating HTML...'));
-};
-
-function init() {
-    inquirer
-        .prompt(
-            questions
-        )
-        .then((response) => {
-            const htmlinfo = (generatehtml(response));
-            writeToFile('index.html', htmlinfo);
-        }
-        );
-};
-
-init();
-
 // initialize a new employee object 
 const newEmployee = new Employee();
 
